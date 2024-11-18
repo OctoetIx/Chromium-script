d7bcea91f47da9c5f6c25e94b2e02042c9e67792
octoetix <iam8nd9@gmail.com>
2024-11-18 15:36:00 +0100

    first commit

---
+++ chromium.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+echo "Welcome to Chromium Setup Script!"
+
+# Update system packages
+sudo apt update && sudo apt upgrade -y
+
+# Check if Screen is installed
+echo "Checking if Screen is installed..."
+if ! command -v screen &> /dev/null; then
+    echo "Screen is not installed. Installing Screen..."
+    sudo apt install screen -y
+else
+    echo "Screen is already installed."
+fi
+
+# Check if Docker is installed
+echo "Checking if Docker is installed..."
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
+    sudo apt update && sudo apt install -y docker.io
+else
+    echo "Docker is already installed."
+fi
+
+docker --version
+
+# Check if Docker Compose is installed
+echo "Checking if Docker Compose is installed..."
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose is not installed. Installing Docker Compose..."
+    sudo apt install -y docker-compose
+else
+    echo "Docker Compose is already installed."
+fi
+
+# Get current timezone from system
+realpath --relative-to /usr/share/zoneinfo /etc/localtime
+
+# Create Chromium Directory
+echo "Creating Chromium Directory..."
+mkdir -p chromium
+cd chromium
+
+# Get custom ports from user or use default ones
+read -p "Enter the port for HTTP (default 3010): " http_port
+http_port=${http_port:-3010}
+read -p "Enter the port for HTTPS (default 3011): " https_port
+https_port=${https_port:-3011}
+
+# Define variables for username and password
+NEW_USERNAME="newuser"
+NEW_PASSWORD="newpassword"
+
+# Replace the username
+sed -i "/username:/s/:.*/: ${NEW_USERNAME}/" docker-compose.yml
+
+# Replace the password
+sed -i "/password:/s/:.*/: ${NEW_PASSWORD}/" docker-compose.yml
+
+echo "Updated username and password in docker-compose.yml"
+
+# Create docker-compose.yaml file
+echo "Creating docker-compose file..."
+cat <<EOF > docker-compose.yaml
+version: "3.7"
+services:
+  chromium:
+    image: lscr.io/linuxserver/chromium:latest
+    container_name: chromium
+    security_opt:
+      - seccomp:unconfined #optional
+    environment:
+      - CUSTOM_USER=$USER
+      - PASSWORD=yourpassword  # Replace with a secure password
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/London  # Replace with your timezone if necessary
+      - CHROME_CLI= https://github.com/OctoetIx # optional
+    volumes:
+      - /root/chromium/config:/config
+    ports:
+      - "$http_port:3000"
+      - "$https_port:3001"
+    shm_size: "1gb"
+    restart: unless-stopped
+EOF
+
+# Display docker-compose.yaml content for review
+cat docker-compose.yaml
+
+# Start the Chromium container
+echo "Starting Chromium container..."
+docker-compose up -d
+
+# Display status
+echo "Docker Chromium is now running."
+echo "You can access it at http://<your-vps-ip>:$http_port"
